74954baeeefc44f495ca881d926b6f88338daf23
Phil Bayfield <phil@bayfmail.com>
2014-10-28 21:02:09 +0000

    remove test code

--- demo/src/main/java/me/philio/disqus/demo/MainActivity.java
+++ demo/src/main/java/me/philio/disqus/demo/MainActivity.java
@@ -49,55 +49,9 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK) {
-            final AccessToken accessToken = data.getParcelableExtra(AuthorizeActivity.EXTRA_ACCESS_TOKEN);
+            AccessToken accessToken = data.getParcelableExtra(AuthorizeActivity.EXTRA_ACCESS_TOKEN);
             Toast.makeText(this, getString(R.string.auth_success, accessToken.username),
                     Toast.LENGTH_LONG).show();
-            new AsyncTask<Void, Void, Void>() {
-                @Override
-                protected Void doInBackground(Void... params) {
-                    try {
-                        ApiConfig config = new ApiConfig(BuildConfig.API_KEY, accessToken.accessToken, BuildConfig.REDIRECT_URI);
-                        ApiClient client = new ApiClient(config);
-
-                        Applications applications = client.createApplications();
-                        Response<Usage> usage = applications.listUsage(null, null);
-                        for (Date date : usage.data.keySet()) {
-                            Log.d(TAG, date.toString() + " " + usage.data.get(date));
-                        }
-
-                        Blacklists blacklists = client.createBlacklists();
-                        Response<List<BlacklistEntry>> domains = blacklists.addDomains("philio", new String[]{"test.com"}, 0, null);
-                        for (BlacklistEntry entry : domains.data) {
-                            Log.d(TAG, entry.type.toString() + " " + entry.value);
-                        }
-                        Response<List<BlacklistEntry>> words = blacklists.addWords("philio", new String[]{"fuck", "fucker", "cunt"}, 0, null);
-                        for (BlacklistEntry entry : words.data) {
-                            Log.d(TAG, entry.type.toString() + " " + entry.value);
-                        }
-                        Response<List<BlacklistEntry>> emails = blacklists.addEmails("philio", new String[]{"test@test.com"}, 0, null);
-                        for (BlacklistEntry entry : emails.data) {
-                            Log.d(TAG, entry.type.toString() + " " + entry.value);
-                        }
-                        Response<List<BlacklistEntry>> ips = blacklists.addIps("philio", new String[]{"254.254.254.254"}, 0, null);
-                        for (BlacklistEntry entry : ips.data) {
-                            Log.d(TAG, entry.type.toString() + " " + entry.value);
-                        }
-                        Response<List<BlacklistEntry>> bUsers = blacklists.addUsers("philio", new Long[]{1l}, 0, null);
-                        for (BlacklistEntry entry : bUsers.data) {
-                            Log.d(TAG, entry.type.toString() + " " + entry.value);
-                        }
-
-                        Users users = client.createUsers();
-                        Response<String> check = users.checkUsername("bayf");
-                        Log.d(TAG, check.data);
-                        Response<UserDetails> details = users.details(null);
-                        Log.d(TAG, details.data.name);
-                    } catch (ApiException e) {
-                        Log.e(TAG, e.getMessage());
-                    }
-                    return null;
-                }
-            }.execute();
         } else {
             Toast.makeText(this, R.string.auth_failure, Toast.LENGTH_LONG).show();
         }
